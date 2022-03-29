2179cde5e0197e70707de0b535d27045fe0357e9
alexflorensa <florensa14@gmail.com>
2022-03-29 11:40:11 +0200

    Added template models

--- manager/models.py
+++ manager/models.py
@@ -1,50 +1,57 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Event(models.Model):
-    name = None
-    initial_date = None
-    final_date = None
+    name = models.CharField(max_length=100, blank=False, null=False)
+    initial_date = models.DateField()
+    final_date = models.DateField()
 
 
 class EventContract(models.Model):
-    file = None
+    file = models.FileField()
+
+
+class StandSize(models.IntegerChoices):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
 
 
 class Stand(models.Model):
-    size = None
+    size = models.IntegerField(choices=StandSize.choices)
 
 
 class StandContract(models.Model):
-    entity = None
-    stand = None
-    file = None
+    entity = models.ForeignKey(User, on_delete=models.SET_NULL)
+    stand = models.ForeignKey(Stand, on_delete=models.SET_NULL)
+    file = models.FileField()
 
 
 class AdditionalService(models.Model):
-    name = None
-    price = None
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2)
 
 
 class EventRequest(models.Model):
-    name = None
-    initial_date = None
-    final_date = None
-    status = None
+    event_name = models.CharField(max_length=100)
+    initial_date = models.DateField()
+    final_date = models.DateField()
+    status = models.BooleanField()
 
 
 class StandRequest(models.Model):
-    entity = None
-    event = None
-    stand = None
-    initial_date = None
-    final_date = None
+    entity = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    stand = models.ForeignKey(Stand, on_delete=models.CASCADE)
+    initial_date = models.DateField()
+    final_date = models.DateField()
 
 
 class ServiceRequest(models.Model):
-    entity = None
-    stand = None
-    service = None
-    event = None
+    entity = models.ForeignKey(User, on_delete=models.CASCADE)
+    stand = models.ForeignKey(Stand, on_delete=models.CASCADE)
+    service = models.ForeignKey(AdditionalService, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
 # TODO: user groups and permissions
