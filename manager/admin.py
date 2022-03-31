dd60132be7ff4823595db03567de9f66f4badfce
Alex Florensa <49005351+alexflorensa@users.noreply.github.com>
2022-03-31 16:21:18 +0200

    Groups permissions (#37)
    * Fixed models, added app and registered to admin site
    
    * Added non tested command
    
    * Added command to setup groups
    
    * Non consuming iteartor

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import (
+    Stand,
+    StandRequest,
+    StandContract,
+    StandSize,
+    AdditionalService,
+    ServiceRequest,
+    Event,
+    EventRequest,
+    EventContract,
+)
+
 # Register your models here.
+admin.site.register(Stand)
+admin.site.register(StandRequest)
+admin.site.register(StandContract)
+
+admin.site.register(AdditionalService)
+admin.site.register(ServiceRequest)
+
+admin.site.register(Event)
+admin.site.register(EventRequest)
+admin.site.register(EventContract)
