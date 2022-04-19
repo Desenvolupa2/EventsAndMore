979f38b6af89314ac0f45dade3dc6adece3d9d7f
alexflorensa <florensa14@gmail.com>
2022-04-19 17:08:04 +0200

    Merged auth branch, fixed urls with / and rearranged templates

--- manager/urls.py
+++ manager/urls.py
@@ -4,10 +4,16 @@ from manager.views import (
     Home,
     Register,
     Login,
+    EventRequestFormView,
+    EventRequestListView,
+    EventRequestStatusUpdate,
 )
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
     path('register/', Register.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
+    path('event-request/', EventRequestFormView.as_view(), name="event-request-form"),
+    path('event-requests/', EventRequestListView.as_view(), name="event-request-list"),
+    path('event-request/<int:pk>/<int:status>', EventRequestStatusUpdate.as_view(), name="event-request-status"),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,8 +1,14 @@
-from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, FormView
+from http import HTTPStatus
 
-from manager.forms import NewUserForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView, ListView, TemplateView, FormView
+
+from manager.forms import EventRequestForm, NewUserForm
+from manager.models import EventRequest, EventRequestStatus
 
 
 class Home(TemplateView):
@@ -22,3 +28,51 @@ class Login(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('home')
+
+
+class EventRequestFormView(LoginRequiredMixin, FormView):
+    template_name = "event_request_form.html"
+    form_class = EventRequestForm
+
+    def form_valid(self, form):
+        event_request: 'EventRequest' = form.save(commit=False)
+        event_request.entity = self.request.user
+        event_request.status = EventRequestStatus.PENDING
+        event_request.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse_lazy("home")
+
+
+class EventRequestListView(LoginRequiredMixin, ListView):
+    template_name = "event_request_list.html"
+    context_object_name = "events"
+    model = EventRequest
+    paginate_by = 10
+    ordering = "initial_date"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context["all_status"] = EventRequestStatus.choices
+        return context
+
+
+class EventRequestStatusUpdate(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(status=HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def post(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        event_request = EventRequest.objects.get(id=pk)
+        if event_request is None:
+            return HttpResponse("Invalid pk", status=HTTPStatus.BAD_REQUEST)
+
+        status = kwargs.get('status')
+        if status is None:
+            return HttpResponse("Invalid status", status=HTTPStatus.BAD_REQUEST)
+
+        event_request.status = EventRequestStatus(status)
+        event_request.save()
+        return HttpResponseRedirect(reverse_lazy("event-request-list"))
