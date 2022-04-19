55dc77f752f687ef985a992ba14752a4b27eba7d
JaumeGuasch <64201574+JaumeGuasch@users.noreply.github.com>
2022-04-19 16:52:57 +0200

    Added register/login functionality (#39)
    * Fixed models, added app and registered to admin site
    
    * Added non tested command
    
    * Modified gitignore
    
    * Added command to setup groups
    
    * Added simple home page and configured urls app router
    
    * Added event request
    
    * List and update event request status
    
    * Groups permissions (#37)
    
    * Fixed models, added app and registered to admin site
    
    * Added non tested command
    
    * Added command to setup groups
    
    * Non consuming iteartor
    
    * Added register functionality
    Added login functionality
    
    * Added AbstractUser
    
    * Changed Login view and updated login templates
    
    * Revert gitignore mp4 file extension
    
    * Updated register view and form
    
    * Removed migrations
    
    * Deleted migrations folder
    
    * Fixed urls and register error not saving users
    
    Co-authored-by: alexflorensa <florensa14@gmail.com>
    Co-authored-by: Alex Florensa <49005351+alexflorensa@users.noreply.github.com>

--- manager/urls.py
+++ manager/urls.py
@@ -1,4 +1,13 @@
+from django.urls import path
+
+from manager.views import (
+    Home,
+    Register,
+    Login,
+)
 
 urlpatterns = [
-
+    path('', Home.as_view(), name="home"),
+    path('register/', Register.as_view(), name='register'),
+    path('login/', Login.as_view(), name='login'),
 ]
