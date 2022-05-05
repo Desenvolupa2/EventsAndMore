0fc9e237c2d6ea2911a60560b5ff6059b8012c47
sebbas0 <64235129+sebbas0@users.noreply.github.com>
2022-05-05 09:58:05 +0200

    profile model

--- manager/models.py
+++ manager/models.py
@@ -3,8 +3,18 @@ from django.db import models
 
 
 class Profile(AbstractUser):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=150, blank=True, null=False)
     # TODO: add all the required fields
+    
+    @receiver(post_save, sender=User)
+	def create_user_profile(sender, instance, created, **kwargs):
+		if created:
+			Profile.objects.create(user=instance)
+
+	@receiver(post_save, sender=User)
+	def save_user_profile(sender, instance, **kwargs):
+		instance.profile.save()
 
 
 class Event(models.Model):
