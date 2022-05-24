35f5e394b9ca0df9381fa41705a6c36b18ac955c
alexflorensa <florensa14@gmail.com>
2022-05-24 22:57:21 +0200

    Fixed typo

--- manager/filters.py
+++ manager/filters.py
@@ -5,11 +5,11 @@ from manager.models import EventRequest, EventRequestStatus
 
 class EventRequestsFilter(django_filters.FilterSet):
 
+    status = django_filters.ChoiceFilter(choices=EventRequestStatus.choices)
+
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.form.fields['status'].widget.attrs.update({'class': 'form-control'})
-
-    status = django_filters.ChoiceFilter(choices=EventRequestStatus.choices)
 
     class Meta:
         model = EventRequest
